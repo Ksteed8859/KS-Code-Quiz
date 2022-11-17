c547da8cab26c74be00de6558a25ed2d8c028e09
Kate Steed <sunfiresteed@yahoo.com>
2022-11-16 21:12:08 -0700

    start button, timer, and question work

--- Assets/script.js
+++ Assets/script.js
@@ -1,81 +1,187 @@
-// Constants and Variables
-const header = document.querySelector(".header");
-const startButton = document.getElementsByClassName("start-button");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const choiceD = document.getElementById("D");
-const timerEl = document.getElementById("timer-count");
-const quizTimer = document.getElementById("quizTimer")
+var start = document.getElementById("start");
+var startButton = document.getElementById("start-btn");
 
-// Questions
+var highscoreLink = document.getElementById("highscore");
+var timerText = document.getElementById("timer-text")
+
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var choices = document.getElementById("choices");
+var a1 = document.getElementById("A");
+var a2 = document.getElementById("B");
+var a3 = document.getElementById("C");
+var a4 = document.getElementById("D");
+var check = document.getElementById("correct-status");
+
+var finishPage = document.getElementById("finish-page");
+var finalScore = document.getElementById("final-score");
+var initials = document.getElementById("initials");
+var submitBtn = document.getElementById("submit-btn");
+var finish = document.getElementById("finish")
+
+var highscorePage = document.getElementById("highscore-page");
+var userRecord = document.getElementById("record");
+var backBtn = document.getElementById("back-btn");
 
 var questions = [
     {
-        question: "Commonly used data types do not include ___________.",
-        choiceA: "Strings",
-        choiceB: "Booleans",
-        choiceC: "Alerts",
-        choiceD: "Numbers",
-        correct : "C"
-    }, {
-        question : "The condition in an if/else statement is inclosed in _________.",
-        choiceA : "Quotes",
-        choiceB: "Curly Brackets",
-        choiceC: "Parenthesis",
-        choiceD: "Square Brackets",
-        correct : "C"
-    }, {
-        question : "Arrays in Javascript can be used to store __________.",
-        choiceA : "Numbers and Strings",
-        choiceB : "Other Arrays",
-        choiceC : "Booleans",
-        choiceD: "All of the Above",
-        correct : "D"
-    }, {
-        question : "String values must be enclosed within __________ when being assigned to variables",
-        choiceA : "Commas",
-        choiceB : "Curly Brackets",
-        choiceC : "Quotes",
-        choiceD: "Parenthesis",
-        correct : "C"
-    }, {
-        question : "A very useful tool used druing development adn debugging for printing content to the debugger is:",
-        choiceA : "Javascript",
-        choiceB : "terminal/bash",
-        choiceC : "for loops",
-        choiceD: "console log",
-        correct : "D"
-    }
+        q: "Commonly used data types do not include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        correct: "c"
+    },
+    {
+        q: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correct: "c"
+    },
+    {
+        q: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correct: "d"
+    },
+    {
+        q: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        correct: "c"
+    },
+    {
+        q: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        correct: "d"
+    },
 ];
 
-const lastQuestion= questions.length - 1;
-var runningQuestion = 0;
+var secondsLeft = 75;
+var questionNumber = 0;
+var score = 0;
+var questionCount = 1;
 
-//Render in a question
 
-function renderQuestion () {
-    let q = questions[runningQuestion];
-    question.innerHTML = "<p>" + q.question + "</p>";
-    choiceA.innerHTML = "<p>" + q.choiceA + "</p>";
-    choiceB.innerHTML = "<p>" + q.choiceB + "</p>";
-    choiceC.innerHTML = "<p>" + q.choiceC + "</p>";
-    choiceD.innerHTML = "<p>" + q.choiceD + "</p>";
+function timerStart() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerText.textContent = "Time: " + secondsLeft + " seconds";
+            if (secondsLeft <=0){
+                clearInterval(timerInterval);
+                timerText.textContent = "Time is up!";
+                finish.textContent = "Time is up!";
+                gameOver();
+            } else if (questionCount >= questionArray.length +1) {
+                clearInterval(timerInterval);
+                gameOver();
+            }
+    }, 1000);
 }
-// Render in timer
-//function renderCounter() {
-    
-//}
 
-//Start Quiz
-
-function startQuiz(){
-    //header.style.display = "none";
-    renderQuestion();
+function startQuiz () {
+    start.style.display = "none";
     quiz.style.display = "block";
-    //quizTimer = setInterval(renderCounter, 1000);
+    questionNumber = 0
+    timerStart();
+    loadQuestion(questionNumber);
+  
+}
+function loadQuestion (n) {
+    question.textContent = questions[n].q;
+    a1.textContent = questions[n].choices[0];
+    a2.textContent = questions[n].choices[1];
+    a3.textContent = questions[n].choices[2];
+    a4.textContent = questions[n].choices[3];
+    questionNumber = n;
 }
 
-startButton.addEventListener("click", startQuiz());
+function checkAnswer(event) {
+    event.preventDefault();
+    check.style.display = "block";
+    setTimeout(function () {
+        check.style.display = "none";
+    }, 1000);
+
+    if (questions[questionNumber].correct == event.target.value) {
+        check.textContent = "Correct";
+        score = score + 1;
+    } else {
+        secondsLeft = secondsLeft - 10;
+        check.textContent = "Incorrect";
+    }
+
+    if (questionNumber < questions.length -1) {
+        loadQuestion(questionNumber +1);
+    } else {
+        quizEnd();
+    }
+    questionCount++;
+}
+
+function quizEnd() {
+    quiz.style.display = "none";
+    finishPage.style.display = "block";
+    finalScore.textContent = "Your final score is: " + score;
+    timerText.style.display = "none";
+}
+
+function getScore() {
+    var currentList =localStorage.getItem("ScoreList");
+    if (currentList !== null ){
+        freshList = JSON.parse(currentList);
+        return freshList;
+    } else {
+        freshList = [];
+    }
+    return freshList;
+}
+
+function addScore() {
+    userRecord.innerHTML = "";
+    userRecord.style.display ="block";
+    var li = document.createElement("li");
+    li.textContent = item.initials + " " + item.score;
+    li.setAttribute("data-index", i);
+    userRecord.appendChild(li);
+}
+
+function addItem (n) {
+    var addedList = getScore()
+    addedList.push(n);
+    localStorage.setItem("ScoreList", JSON.stringify(addedList));
+}
+function saveScore () {
+    var scoreItem ={
+        user: userInitial.value,
+        score: totalScore
+    }
+    addItem(scoreItem);
+    renderScore();
+}
+
+startButton.addEventListener("click", startQuiz);
+choices.forEach(function(click) {
+    click.addEventListener("chick", checkAnswer);
+});
+
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    finishPage.style.display = "none";
+    start.style.display = "none";
+    highscorePage.style.display = "block";
+    quiz.style.display = "none";
+    saveScore()
+});
+
+highscoreLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    finishPage.style.display = "none";
+    start.style.display = "none";
+    highscorePage.style.display = "block";
+    quiz.style.display ="none";
+    renderScore();
+});
+
+backBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    finishPage.style.display = "none";
+    start.style.display = "block";
+    highscorePage.style.display = "none";
+    quiz.style.display ="none";
+    location.reload();
+});
